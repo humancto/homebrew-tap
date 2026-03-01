c0ccf953a880a5b8b167c81427d121ff1e746566
Archy <archith.rapaka+github@gmail.com>
2026-03-01 23:29:34 +0530

    bump forge to v0.3.1 (rebuilt with version fix)

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -7,20 +7,20 @@ class Forge < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/humancto/forge-lang/releases/download/v0.3.1/forge-v0.3.1-aarch64-apple-darwin.tar.gz"
-      sha256 "090acb049320011e044cc4d64a31ea1d6e9fa1205834a5f6fb54605e09270df1"
+      sha256 "15b25a7627840eb3c104d59bcf740b80b91fc0a3277e3ea06a43b97e0fe2c80f"
     else
       url "https://github.com/humancto/forge-lang/releases/download/v0.3.1/forge-v0.3.1-x86_64-apple-darwin.tar.gz"
-      sha256 "49de9bb79da7f021b572e01524d3cd0a9eddb36ea50056dca7304042773d1f27"
+      sha256 "741f76cf3172e043bc4bbdeb691812b2d9226ebb1e11a098b2097f4958169d8a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/humancto/forge-lang/releases/download/v0.3.1/forge-v0.3.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "6c24e9bdbe6bc742ce370a47273a755f9bfd6caba88c1b4e499f58468c4fa976"
+      sha256 "42447051e0c1a3159a4c3e6ff15f48be5bd4fb030f543c0cb13337a7d0d9f6d2"
     else
       url "https://github.com/humancto/forge-lang/releases/download/v0.3.1/forge-v0.3.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "85a822a9c5ece7418d3889fc2866dfd1ff7b59ad9e06b407dd9495e3ff4da524"
+      sha256 "d6a2972fedf6f2c78479343fa45ab85176e17572865b7e328d534fd15fd0e943"
     end
   end
 
