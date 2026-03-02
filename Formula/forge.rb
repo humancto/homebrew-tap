d345296ff594c989f620a4e1748142e0ec6133a2
Archy <archith.rapaka+github@gmail.com>
2026-03-02 12:49:06 +0530

    chore: bump forge to v0.3.3 (type system + landing page updates)

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,26 +1,26 @@
 class Forge < Formula
   desc "Internet-native programming language with natural syntax and Cranelift JIT"
   homepage "https://github.com/humancto/forge-lang"
-  version "0.3.2"
+  version "0.3.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/humancto/forge-lang/releases/download/v0.3.2/forge-v0.3.2-aarch64-apple-darwin.tar.gz"
-      sha256 "16ec053105c0a1b461d26222dbaec21f4840e69b10f83bba568c7b424b95275a"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.3.3/forge-v0.3.3-aarch64-apple-darwin.tar.gz"
+      sha256 "464ad7475210745ca965c3c7741ace3fa6635c5109653c4a14240f85d5f7e776"
     else
-      url "https://github.com/humancto/forge-lang/releases/download/v0.3.2/forge-v0.3.2-x86_64-apple-darwin.tar.gz"
-      sha256 "3aa71d878f0f8f1868c03c07c7d0c6a1cfef7b13541d431182893bcc04f3a0c1"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.3.3/forge-v0.3.3-x86_64-apple-darwin.tar.gz"
+      sha256 "598c86d9f19b62cdf9fdbe216266ec35b7e8f74984efc32b1d5b33b778233ed9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/humancto/forge-lang/releases/download/v0.3.2/forge-v0.3.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3954dc835abf02854a3e54fac288c176de8997dd041eda9a29838e492ec803c8"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.3.3/forge-v0.3.3-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "60d40d26aa6130868bcde45b58500790d20169d3e5d27f6626122a9e4732ba7a"
     else
-      url "https://github.com/humancto/forge-lang/releases/download/v0.3.2/forge-v0.3.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "180c0839b63643acce6914058401f52390e9f3baabb75b3852a472d60344b5f4"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.3.3/forge-v0.3.3-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d3cc986bccb736cce208dad8cdee3bed82f65c7823f6b123f879666e8dfe7b31"
     end
   end
 
@@ -32,4 +32,3 @@ class Forge < Formula
     assert_match "Forge v", shell_output("#{bin}/forge --version")
   end
 end
-
