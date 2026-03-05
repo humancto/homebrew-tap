40e76f2f51ec96baf59923ed8a1da0197ab7b3b1
Archy <archith.rapaka+github@gmail.com>
2026-03-06 02:43:51 +0530

    chore: bump forge formula to v0.4.2

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,26 +1,26 @@
 class Forge < Formula
   desc "Internet-native programming language with natural syntax and Cranelift JIT"
   homepage "https://github.com/humancto/forge-lang"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.1/forge-v0.4.1-aarch64-apple-darwin.tar.gz"
-      sha256 "0d129841484c8b356a7acf9794f5934664fd6a779446a3d02e9d8b5897298bf7"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.4.2/forge-v0.4.2-aarch64-apple-darwin.tar.gz"
+      sha256 "493cedf5a2496198952b6a5b95182e0e539434fbf066916498c8bb813d2f2f47"
     else
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.1/forge-v0.4.1-x86_64-apple-darwin.tar.gz"
-      sha256 "c1331e307d650dc9f6768299ac8b1e92eec9d5065d6e8098b47fe3a0d19d4a1e"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.4.2/forge-v0.4.2-x86_64-apple-darwin.tar.gz"
+      sha256 "350142a7cc022fc0bd035cd87a230e23f0076b0805f983e1d3bd5119031e1eda"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.1/forge-v0.4.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "426aca5def73028d83a0299be3cf32d1660660fe58fab14fe4b2cb7521ac2555"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.4.2/forge-v0.4.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f50d98912bf27139fab513caca6e52a322498ac05b11b401ed18435e19cc079f"
     else
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.1/forge-v0.4.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2087e08f1f3a63a5a6b0c644d3eb44605476e058c358685e9f9f340669257380"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.4.2/forge-v0.4.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "b9784460af64e659e212eef84715c7227a0b6814f4f86d0798d031f74586180c"
     end
   end
 
