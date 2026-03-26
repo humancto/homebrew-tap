9a855b2e7aae6c334ecf38647d2aa5be740d8235
Archy <archith.rapaka+github@gmail.com>
2026-03-26 21:17:31 +0000

    Update punch to v1.2.0

--- Formula/punch.rb
+++ Formula/punch.rb
@@ -1,22 +1,22 @@
 class Punch < Formula
   desc "The Agent Combat System — deploy autonomous AI agent squads from a single binary"
   homepage "https://github.com/humancto/punch"
-  version "1.1.0"
+  version "1.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/humancto/punch/releases/download/v1.1.0/punch-aarch64-apple-darwin.tar.gz"
-      sha256 "9576e8ffcd4ede18da2bb22c1e907d404839a577e7434d29161b335077e218f1"
+      url "https://github.com/humancto/punch/releases/download/v1.2.0/punch-aarch64-apple-darwin.tar.gz"
+      sha256 "62f20a188d5003451b05d3e5b892b5f2538c393de417b949e4f97fe36659ae0d"
     else
-      url "https://github.com/humancto/punch/releases/download/v1.1.0/punch-x86_64-apple-darwin.tar.gz"
-      sha256 "e66719de2d75161463ee9d15c891ae0c0ff8f3b986fc9618d1ee97338f49dc77"
+      url "https://github.com/humancto/punch/releases/download/v1.2.0/punch-x86_64-apple-darwin.tar.gz"
+      sha256 "7145fc7dfdfb099c9d3d4ab68e3762980a46971c49c4de9c7033679c13a30d1a"
     end
   end
 
   on_linux do
-    url "https://github.com/humancto/punch/releases/download/v1.1.0/punch-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "a3173021057316d815c90b1ba838d8244fa3f6aeea41c232907668253cf784d9"
+    url "https://github.com/humancto/punch/releases/download/v1.2.0/punch-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "9644e514a750cb3d74ce6efc528104d4b80abeb63126cb5a41307116c6a83f4b"
   end
 
   def install
