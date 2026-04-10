d28a07ff3490e5aa1ef373a3250688c5b894cc11
Archy <archith.rapaka+github@gmail.com>
2026-04-10 13:32:45 -0700

    forge 0.5.0

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,26 +1,26 @@
 class Forge < Formula
   desc "Internet-native programming language with natural syntax and Cranelift JIT"
   homepage "https://github.com/humancto/forge-lang"
-  version "0.4.3"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.3/forge-v0.4.3-aarch64-apple-darwin.tar.gz"
-      sha256 "9f05da7fd1e0d0f810486f2e8a2a0fb7b0afc6bef775e2edc6eb46989bc9af43"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.5.0/forge-v0.5.0-aarch64-apple-darwin.tar.gz"
+      sha256 "d0502ebb5df37b1c06bdc569c335556ea89a85eb01e134e3fb2611831c263952"
     else
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.3/forge-v0.4.3-x86_64-apple-darwin.tar.gz"
-      sha256 "6c428bf2abc82557d44d6bac82f507791e0c567ef02661fc6d4bf97474d283cf"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.5.0/forge-v0.5.0-x86_64-apple-darwin.tar.gz"
+      sha256 "10769c8155811100a8a92772263502f89bd05b54dde81dc23936f27b53b9168d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.3/forge-v0.4.3-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "45a97170618f621cc04f6e18eadabe5c92e75fca1aa4158f0e1c562c9f221ba7"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.5.0/forge-v0.5.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "80c8fa05e8f520e536bc4eec95b5c542f6d9d90aa369a1d4a26ae529b12884a2"
     else
-      url "https://github.com/humancto/forge-lang/releases/download/v0.4.3/forge-v0.4.3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9b5cd03a0a1b60aedec73a4052e5a16ecb52a420caaf35895d39e291b835b40a"
+      url "https://github.com/humancto/forge-lang/releases/download/v0.5.0/forge-v0.5.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "3683e9c745d29027b9d95b877f1c7336377ad6b1cd518d7b2d908d7471b1fb07"
     end
   end
 
